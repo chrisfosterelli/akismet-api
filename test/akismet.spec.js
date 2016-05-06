768f74521fa760222231cd4ec63ac12e2941579e
Chris Foster <chris.james.foster@gmail.com>
2016-05-06 11:02:56 -0700

    Actually, according to their API docs we don't need to pass the server erro r message

--- test/akismet.spec.js
+++ test/akismet.spec.js
@@ -179,7 +179,7 @@ describe('Akismet-api', function() {
         client = Akismet.client({
           blog : 'http://example.com',
           key  : 'testKey3',
-          host : 'localhost' // will fail!
+          host : 'notarealdomain' // will fail!
         });
       });
 
@@ -193,6 +193,7 @@ describe('Akismet-api', function() {
 
       it('should return the error', function(done) {
         client.verifyKey(function(err, valid) {
+          expect(err.message).to.include('ENOTFOUND');
           expect(err).to.not.be.null;
           done();
         });
@@ -382,7 +383,7 @@ describe('Akismet-api', function() {
         client = Akismet.client({
           blog : 'http://example.com',
           key  : 'testKey7',
-          host : 'localhost' // will fail!
+          host : 'notarealdomain' // will fail!
         });
       });
 
@@ -429,7 +430,7 @@ describe('Akismet-api', function() {
         scope = nock('http://testKey8.rest.akismet.com')
         .matchHeader('Content-Type', 'application/x-www-form-urlencoded')
         .post('/1.1/submit-spam')
-        .reply(200, 'Thank you for making the internet a better place', {
+        .reply(200, 'Thanks for making the web a better place.', {
           'Content-Type' : 'text/plain'
         });
       });
@@ -459,7 +460,7 @@ describe('Akismet-api', function() {
         scope = nock('http://testKey9.rest.akismet.com')
         .matchHeader('Content-Type', 'application/x-www-form-urlencoded')
         .post('/1.1/submit-spam')
-        .reply(500, 'Oh, whoops.', {
+        .reply(500, {
           'Content-Type' : 'text/plain'
         });
       });
@@ -468,7 +469,7 @@ describe('Akismet-api', function() {
         client.submitSpam({
           user_ip : '123.123.123.123'
         }, function(err) {
-          expect(err.message).to.equal('Oh, whoops.');
+          expect(err.message).to.equal('Internal Server Error');
           scope.done();
           done();
         });
@@ -484,7 +485,7 @@ describe('Akismet-api', function() {
         client = Akismet.client({
           blog : 'http://example.com',
           key  : 'testKey10',
-          host : 'localhost' // will fail!
+          host : 'notarealdomain' // will fail!
         });
       });
 
@@ -522,7 +523,7 @@ describe('Akismet-api', function() {
         scope = nock('http://testKey11.rest.akismet.com')
         .matchHeader('Content-Type', 'application/x-www-form-urlencoded')
         .post('/1.1/submit-ham')
-        .reply(200, 'Thank you for making the internet a better place', {
+        .reply(200, 'Thanks for making the web a better place.', {
           'Content-Type' : 'text/plain'
         });
       });
@@ -552,7 +553,7 @@ describe('Akismet-api', function() {
         scope = nock('http://testKey12.rest.akismet.com')
         .matchHeader('Content-Type', 'application/x-www-form-urlencoded')
         .post('/1.1/submit-ham')
-        .reply(500, 'Oh, whoops.', {
+        .reply(500, {
           'Content-Type' : 'text/plain'
         });
       });
@@ -561,7 +562,7 @@ describe('Akismet-api', function() {
         client.submitHam({
           user_ip : '123.123.123.123'
         }, function(err) {
-          expect(err.message).to.equal('Oh, whoops.');
+          expect(err.message).to.equal('Internal Server Error');
           scope.done();
           done();
         });
@@ -577,7 +578,7 @@ describe('Akismet-api', function() {
         client = Akismet.client({
           blog : 'http://example.com',
           key  : 'testKey13',
-          host : 'localhost' // will fail!
+          host : 'notarealdomain' // will fail!
         });
       });
 
@@ -585,6 +586,7 @@ describe('Akismet-api', function() {
         client.submitHam({
           user_ip : '123.123.123.123'
         }, function(err) {
+          expect(err.message).to.include('ENOTFOUND');
           expect(err).to.not.be.null;
           done();
         });
